d32b18a6afcb866ad8e967411d96a1ece80ded4c
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-12-04 22:21:04 +0600

    add new ulr for experiment

--- django_ehelp/views.py
+++ django_ehelp/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def index(request):
   return HttpResponse("Hellow world Django")
+
+def hello(request):
+  return HttpResponse('Well come to Django learning')
