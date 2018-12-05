e7d764af4a4bdd6fd6d2c19a31c850d9aa38c2c2
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-12-05 22:43:09 +0600

    create migration for ticket

--- ticket/models.py
+++ ticket/models.py
@@ -5,9 +5,9 @@ from django.utils import timezone
 # Create your models here.
 class Ticket(models.Model):
   creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
-  title = models.CharField(max=50)
-  description = models.CharField(max=200, min=10)
-  status = models.CharField()
+  title = models.CharField(max_length=50)
+  description = models.CharField(max_length=200)
+  status = models.CharField(max_length=20)
   created_at = models.DateTimeField(default=timezone.now)
 
   def mark_done(self):
