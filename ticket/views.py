78b3fe00f07c11f74213b70c36b034589767cbd6
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-12-06 00:01:21 +0600

    add some html to view

--- ticket/views.py
+++ ticket/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def ticket_list(request):
-  return HttpResponse("Welcome to you custom url")
+  return render(request, 'ticket/index.html', {})
