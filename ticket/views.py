35a0818a7e7b3754b2aa0c9e51201cfdbc74709d
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-12-06 22:28:08 +0600

    show ticke on template

--- ticket/views.py
+++ ticket/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Ticket
 
 # Create your views here.
 def ticket_list(request):
-  return render(request, 'ticket/index.html', {})
+  tickets = Ticket.objects.all()
+  return render(request, 'ticket/index.html', {'tickets': tickets})
