9e1ffddd57183dcbbacd1ceef3eb5279f3232fff
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-12-08 21:05:23 +0600

    add form class for new ticket

--- ticket/urls.py
+++ ticket/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
   path('', views.ticket_list, name='ticket_index'),
-  path('ticket/<int:pk>', views.show, name='show_ticket')
+  path('ticket/<int:pk>', views.show, name='show_ticket'),
+  path('/tickets/new', views.new, name='new_ticket')
 ]
--- ticket/views.py
+++ ticket/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Ticket
+from .forms import TicketForm
 
 # Create your views here.
 def ticket_list(request):
@@ -10,3 +11,7 @@ def ticket_list(request):
 def show(request, pk):
   ticket = Ticket.objects.get(pk=pk)
   return render(request, 'ticket/show.html', {'ticket': ticket})
+
+def new(request):
+  form = TicketForm()
+  return render(request, 'ticket/new.html', {'form': form})
