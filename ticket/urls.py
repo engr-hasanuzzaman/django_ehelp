1ed549f9d822b1a4f0e9aa6833e3ec9fd85a31a5
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-12-08 21:10:01 +0600

    correct urlSSS

--- ticket/urls.py
+++ ticket/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
   path('', views.ticket_list, name='ticket_index'),
   path('ticket/<int:pk>', views.show, name='show_ticket'),
-  path('/tickets/new', views.new, name='new_ticket')
+  path('tickets/new/', views.new, name='new_ticket')
 ]
