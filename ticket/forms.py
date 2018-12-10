f5e21f4ddc551ea658b6d1585ecf36b2cb324f7d
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-12-10 23:50:16 +0600

    add custom class to all form field

--- ticket/forms.py
+++ ticket/forms.py
@@ -5,3 +5,8 @@ class TicketForm(forms.ModelForm):
   class Meta:
     model = Ticket
     fields = ('title', 'description', 'status')
+    widgets = {
+          'title': forms.TextInput(attrs={'class': 'form-control'}),
+          'description': forms.Textarea(attrs={'class': 'form-control'}),
+          'status': forms.TextInput(attrs={'class': 'form-control'})
+        }
