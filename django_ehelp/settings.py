ec59d457401abc4c3b5865c301dbd6531a61cb53
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-12-11 00:15:44 +0600

    add rest to django app

--- django_ehelp/settings.py
+++ django_ehelp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'ticket'
 ]
 
@@ -120,4 +121,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
