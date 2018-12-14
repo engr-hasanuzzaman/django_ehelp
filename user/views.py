c1d1e8e7db1e699becb997ce1d25f21b512359ff
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-12-14 22:31:10 +0600

    add 403 on user create temporarity

--- user/views.py
+++ user/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, authenticate
+from django.conf.urls import handler403
 
 # Create your views here.
 def sign_up(request):
   if request.method == 'POST':
     # user registration
-    return None
+    return handler403(request, None)
   else:
     form = UserCreationForm()
     return render(request, 'user/new.html', {'form': form})
