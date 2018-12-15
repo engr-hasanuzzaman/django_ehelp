a460acabf1f7b6a3bf54e742b82cd5fb67f808b2
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-12-15 23:41:10 +0600

    add label to django form

--- user/forms.py
+++ user/forms.py
@@ -2,5 +2,5 @@ from django import forms
 from django.contrib.auth.models import User
 
 class UserLoginForm(forms.Form):
-  username = forms.CharField(label='test')
-  password = forms.CharField(label='password', widget=forms.PasswordInput())
+  username = forms.CharField(label='test', widget=forms.TextInput(attrs={'class': 'form-control'}))
+  password = forms.CharField(label='password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
