3ccbfdb153281ff388b7a9bb71e280e08c2049d3
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-12-16 00:24:37 +0600

    create basic user rest api

--- django_ehelp/urls.py
+++ django_ehelp/urls.py
@@ -14,15 +14,35 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import url
+from django.conf.urls import url, re_path
 from django.urls import path, include, re_path
 from . import views
 from rest_framework import routers, serializers, viewsets
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
 
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'is_staff')
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('super/', admin.site.urls),
     re_path('^ticket', include('ticket.urls')),
     re_path(r'accounts/', include('user.urls')),
     path('', views.home, name='home'),
-    re_path(r'^api/', include('rest_framework.urls'))
+    re_path(r'^api/v[1 2 3]/', include(router.urls)),
+    re_path(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
