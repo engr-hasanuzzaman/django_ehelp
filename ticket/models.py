3a393c3b9740bae046f7e945d1c6ea472f5df11c
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-12-21 23:59:31 +0600

    use enum with int field for ticket status

--- ticket/models.py
+++ ticket/models.py
@@ -1,13 +1,20 @@
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
+from model_utils import Choices
 
 # Create your models here.
 class Ticket(models.Model):
+  STATUSES = Choices(
+        (0, 'open', 'Open'),
+        (1, 'reviewed', 'Reviewed by Manager'),
+        (2, 'closed', 'Closed'),
+      )
+
   creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
   title = models.CharField(max_length=50)
   description = models.CharField(max_length=200)
-  status = models.CharField(max_length=20)
+  status = models.IntegerField(default=STATUSES.open, choices=STATUSES)
   created_at = models.DateTimeField(default=timezone.now)
 
   def mark_done(self):
@@ -17,10 +24,3 @@ class Ticket(models.Model):
   def __str__(self):
     return self.title
   
-  
-# declare enum for ticket type 
-from enum import Enum
-class Status(Enum):   # A subclass of Enum
-    opn = "Open"
-    cls = "Close"
-    re = "Reviewd"
